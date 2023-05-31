508b15c16611a7cbfbb5c84da7e833c5f74aa749
joaopachiel <joaopachiel@gmail.com>
2023-05-31 11:58:37 -0300

    Add project files.

---
+++ ProjetoExercitoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoExercitoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
