3293f38914b03cd0cfe38fab3df2d69ce29604f5
joaopachiel <joaopachiel@gmail.com>
2023-06-14 16:45:39 -0300

    Criação da classe ProjetoExercitoMvcContext e configurações de string de conexão.

---
+++ ProjetoExercitoMVC/Data/Context/ProjetoExercitoMvcContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ProjetoExercitoMVC.Models.Entity;
+
+namespace ProjetoExercitoMVC.Data.Context
+{
+    public class ProjetoExercitoMvcContext : DbContext
+    {
+        public ProjetoExercitoMvcContext(DbContextOptions<ProjetoExercitoMvcContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Companhia> Companhia { get; set; }
+        public DbSet<Militar> Militar { get; set; }
+    }
+}
