a2b1052e63acbb8113d0e03f127d371791044b6e
joaopachiel <joaopachiel@gmail.com>
2023-06-16 16:45:09 -0300

    Novos métodos de buscas de militares, buscar todos e buscar por id, e método de editar militar.

--- ProjetoExercitoMVC/Services/MilitarService.cs
+++ ProjetoExercitoMVC/Services/MilitarService.cs
@@ -1,4 +1,5 @@
-﻿using ProjetoExercitoMVC.Data.Context;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoExercitoMVC.Data.Context;
 using ProjetoExercitoMVC.Models.Entity;
 
 namespace ProjetoExercitoMVC.Services
@@ -12,9 +13,31 @@ namespace ProjetoExercitoMVC.Services
             _context = context;
         }
 
+        public List<Militar> BuscarTodosMilitares()
+        {
+            return _context.Militar.OrderBy(x => x.Nome).ToList();
+        }
+
+        public Militar BuscarMilitarPorId(int id)
+        {
+            return _context.Militar.Include(x => x.Companhia).FirstOrDefault(x => x.Id == id);
+        }
+
         public void AdicionarMilitar(Militar militar)
         {
             _context.Add(militar);
+            _context.SaveChanges();
+        }
+
+        public void EditarMilitar(Militar militar)
+        {
+            _context.Update(militar);
+            _context.SaveChanges();
+        }
+
+        public void DeletarMilitar(Militar militar)
+        {
+            _context.Remove(militar);
             _context.SaveChanges();
         }
     }
