aae987beddfff258ef1b907cf2ae5660d6f6e922
joaopachiel <joaopachiel@gmail.com>
2023-07-31 12:46:11 -0300

    Feature: ajuste em MilitarViewModel(tratamentos de dados foram passados da classe model para a viewModel) -> classe model mais limpa.

--- ProjetoExercitoMVC/Models/Entity/Militar.cs
+++ ProjetoExercitoMVC/Models/Entity/Militar.cs
@@ -7,21 +7,10 @@ namespace ProjetoExercitoMVC.Models.Entity
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
-        [Display(Name = "Data de ingresso")]
         public DateTime DataIngresso { get; set; }
-
-        [Display(Name = "Graduação")]
         public string Graduacao { get; set; }
-
         public Companhia Companhia { get; set; }
-
-        [Display(Name = "Companhia")]
         public int CompanhiaId { get; set; }
-
-        [Display(Name = "Função")]
         public string FuncaoExercida { get; set; }
 
         public Militar()
--- ProjetoExercitoMVC/Models/ViewModel/MilitarViewModel.cs
+++ ProjetoExercitoMVC/Models/ViewModel/MilitarViewModel.cs
@@ -1,4 +1,5 @@
 using ProjetoExercitoMVC.Models.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoExercitoMVC.Models.ViewModel
 {
@@ -6,6 +7,24 @@ namespace ProjetoExercitoMVC.Models.ViewModel
     public class MilitarViewModel
     {
         public Militar Militar { get; set; }
+
+        public string Nome { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Data de ingresso")]
+        public DateTime DataIngresso { get; set; }
+
+        [Display(Name = "Graduação")]
+        public string Graduacao { get; set; }
+
+        public Companhia Companhia { get; set; }
+
+        [Display(Name = "Companhia")]
+        public int CompanhiaId { get; set; }
+
+        [Display(Name = "Função")]
+        public string FuncaoExercida { get; set; }
         public ICollection<Companhia> Companhias { get; set; }
     }
 }
